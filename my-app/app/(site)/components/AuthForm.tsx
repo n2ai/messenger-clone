60efc02bf3372697edc01f5d7415d802b14b0162
n2ai <longhai2511@gmail.com>
2023-09-24 23:14:36 -0500

    Update

---
+++ my-app/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+type Variant = 'LOGIN' | 'REGISTER'
+const AuthForm = ()=>{
+    const [variant,setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(()=>{
+        if(variant === 'LOGIN'){
+            setVariant('REGISTER')
+        }else{
+            setVariant('LOGIN')
+        }
+    },[variant])
+
+    const {
+        register,
+        handleSubmit,
+        formState:{
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues:{
+            name:'',
+            email:'',
+            password:''
+        }
+    })
+
+    const onSubmit:SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true)
+        if(variant === 'REGISTER'){
+            //axios register
+        }
+
+        if(variant === 'LOGIN'){
+            //NextAuth SignIn
+        }
+    }
+
+    const socialAction = (action:string)=>{
+        setIsLoading(true);
+        //NextAuth Social Sign In
+    }
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default AuthForm
--- my-app/app/(site)/page.tsx
+++ my-app/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import AuthForm from "./components/AuthForm"
 
 export default function Home() {
     return (
@@ -7,7 +8,7 @@ export default function Home() {
             <Image alt='Logo' height="48" width="48" className="mx-auto w-auto" src="/images/logo.png"></Image>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
-        {/**Auth Form */}
+        <AuthForm />
       </div>
     )
 }
