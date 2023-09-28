8c2f94f1cd295064167fe477130ab17424bac592
n2ai <longhai2511@gmail.com>
2023-09-28 01:45:26 -0500

    update social medias icon

--- my-app/app/(site)/components/AuthForm.tsx
+++ my-app/app/(site)/components/AuthForm.tsx
@@ -1,8 +1,10 @@
 'use client';
 import { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import {BsGithub, BsGoogle} from 'react-icons/bs'
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
 type Variant = 'LOGIN' | 'REGISTER'
 const AuthForm = ()=>{
     const [variant,setVariant] = useState<Variant>('LOGIN');
@@ -59,11 +61,46 @@ const AuthForm = ()=>{
                     <Input id="password" label="Password" type="password" register={register} errors={errors}>
                     </Input>
                     <div>
-                        <Button>
-                            Test
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >
+                            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
                         </Button>
                     </div>
                 </form>
+
+                <div className="mt-6">
+                    <div className="relative">
+                        <div 
+                            className="
+                            absolute 
+                            inset-0 
+                            flex 
+                            items-center
+                            "
+                        >
+                            <div className="w-full border-t border-gray-300" />
+                            </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton
+                            icon={BsGithub}
+                            onClick={()=>socialAction('github')}
+                        />
+                        <AuthSocialButton
+                            icon={BsGoogle}
+                            onClick={()=>socialAction('google')}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
