943e792ac47d60c25a0a84b3879e393a3b0d4ac7
n2ai <longhai2511@gmail.com>
2023-10-02 22:06:45 -0500

    Update login

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
-        <ToasterContext/>
-        {children}</body>
+        <AuthContext>
+          <ToasterContext/>
+          {children}
+        </AuthContext>
+        </body>
     </html>
   )
 }
