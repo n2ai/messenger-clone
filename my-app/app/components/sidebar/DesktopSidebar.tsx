14a31bd0bf76297206761280deb81e9aa601efc0
n2ai <longhai2511@gmail.com>
2023-10-05 23:02:07 -0500

    Update routes

---
+++ my-app/app/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import useRoutes from "@/app/hooks/useRoutes"
+import { useState } from "react"
+const DesktopSidebar = ()=>{
+
+    const routes = useRoutes()
+    const [isOpen,setIsOpen] = useState(false)
+
+    return (
+        <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+            <nav className="mt-4 flex flex-col">
+                <ul role="list" className="flex flex-col itmes-center space-y-1">
+                    {routes.map((item)=>{
+                        //Create desktop Item
+                        <DesktopItem key={item.label} href={item.href} label={item.label} icon={item.icon} active={item.active} onClick={item.onClick}>
+
+                        </DesktopItem>
+                    })}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default DesktopSidebar
