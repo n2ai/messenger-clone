ff144c4c4f8e4388511dfc6066cdb6646ca3e41f
n2ai <longhai2511@gmail.com>
2023-10-09 18:29:19 -0500

    Finish sidebar

--- my-app/app/components/sidebar/DesktopSidebar.tsx
+++ my-app/app/components/sidebar/DesktopSidebar.tsx
@@ -12,7 +12,7 @@ const DesktopSidebar = ()=>{
     return (
         <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
             <nav className="mt-4 flex flex-col">
-                <ul role="list" className="flex flex-col itmes-center space-y-1">
+                <ul role="list" className="flex flex-col items-center space-y-1">
                     {routes.map((item)=>(
                         //Create desktop Item
                         <DesktopItem key={item.label} href={item.href} label={item.label} icon={item.icon} active={item.active} onClick={item.onClick}>
--- my-app/app/components/sidebar/Sidebar.tsx
+++ my-app/app/components/sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
 import DesktopSidebar from "./DesktopSidebar"
+import MobileFooter from "./MobileFooter"
 async function Sidebar({children}:{children:React.ReactNode}){
     return (
         <div className="h-full">
             <DesktopSidebar></DesktopSidebar>
+            <MobileFooter></MobileFooter>
             <main className="lg:pl-20 h-full">
                 {children}
             </main>
