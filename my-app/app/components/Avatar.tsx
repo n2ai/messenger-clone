4a7b26b0b46fc07f0c863ae2ebda8389f7a730da
n2ai <longhai2511@gmail.com>
2023-10-11 01:02:38 -0500

    Update user

--- my-app/app/components/Avatar.tsx
+++ my-app/app/components/Avatar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { User } from "@prisma/client";
+import Image from "next/image";
 
 interface AvatarProps{
     user?:User;
@@ -11,10 +12,15 @@ const Avatar:React.FC<AvatarProps> = ({
 })=>{
     return(
         <div className="relative">
+            {/**Get image from github :> placeholder.jpg */}
             <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
-                
+                <Image fill alt="Avatar" src={user?.image || '/images/placeholder.jpg'}></Image>
             </div>
+            <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3">
+
+            </span>
         </div>
+        
     )
 }
 
--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        appDir:true,
-        swcPlugins:[
-            ["next-superjson-plugin",{}]
+    images:{
+        domains:[
+            "res.cloudinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com"
         ]
     }
 }
